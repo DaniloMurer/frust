da465229017a3fddb51b2d754e68093ace4921c9
Danilo Jakob <danilojakob@protonmail.com>
2024-08-16 20:07:42 +0200

    dev: learning rust

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,8 @@
-fn main() {
-    println!("Hello, world!");
+use std::io;
 
+fn main() {
+    println!("guess the number");
+    let mut guess = String::new();
+    io::stdin().read_line(&mut guess).expect("Failed to read line?");
+    println!("your input: {}", guess);
 }
