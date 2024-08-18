ab0481c7c9df5c4bf25d5a6690dedab42b550efb
Danilo Jakob <danilojakob@protonmail.com>
2024-08-18 15:26:54 +0200

    dev: guessing game variable size improvement

--- src/main.rs
+++ src/main.rs
@@ -3,14 +3,14 @@ use std::{cmp::Ordering, io};
 
 fn main() {
     println!("guess the number");
-    let random_number = thread_rng().gen_range(1..=20);
+    let random_number: u8 = thread_rng().gen_range(1..=20);
     let mut has_guessed = false;
     while !has_guessed {
         let mut guess = String::new();
         io::stdin()
             .read_line(&mut guess)
             .expect("Failed to read line?");
-        let guess: i32 = guess.trim().parse().expect("Error while casting");
+        let guess: u8 = guess.trim().parse().expect("Error while casting");
 
         match guess.cmp(&random_number) {
             Ordering::Less => println!("the number is bigger"),
