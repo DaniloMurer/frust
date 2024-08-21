90d3287c7f11ed5cf3011f03c0f97c4560a2d7be
Danilo Jakob <danilojakob@protonmail.com>
2024-08-21 18:37:34 +0200

    dev: go-ops implementation in rust

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,4 @@
-// use rand::{thread_rng, Rng};
-// use std::{cmp::Ordering, io};
-
-use std::process::Command;
+use std::{os::linux::raw::stat, process::{Command, ExitStatus}};
 
 /// [`User`]
 ///
@@ -24,25 +21,13 @@ fn main() {
     println!("User's age is: {}", calculate_age(&user));
     let output = Command::new("ls").output().expect("Failed to run command");
     println!("{}", String::from_utf8_lossy(&output.stdout));
-    // println!("guess the number");
-    // let random_number: u8 = thread_rng().gen_range(1..=20);
-    // let mut has_guessed = false;
-    // while !has_guessed {
-    //     let mut guess = String::new();
-    //     io::stdin()
-    //         .read_line(&mut guess)
-    //         .expect("Failed to read line?");
-    //     let guess: u8 = guess.trim().parse().expect("Error while casting");
-    //
-    //     match guess.cmp(&random_number) {
-    //         Ordering::Less => println!("the number is bigger"),
-    //         Ordering::Greater => println!("the number is smaller"),
-    //         Ordering::Equal => {
-    //             println!("you guess right: {}", guess);
-    //             has_guessed = true;
-    //         }
-    //     }
-    // }
+    println!("{}", &output.status);
+    let commit_status = commit_changes();
+}
+
+fn commit_changes() -> ExitStatus {
+    let commit = Command::new("git").args(["commit", "-am", "feat: bumped version"]).status();
+    commit.expect("")
 }
 
 fn calculate_age(user: &User) -> usize {
