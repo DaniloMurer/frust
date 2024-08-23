c9db2417f1f0ebb46a1e768b16755bed49ab1402
Danilo Jakob <danilojakob@protonmail.com>
2024-08-23 22:13:09 +0200

    feat: implemented writing to file and correct git handling

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,5 @@
 use std::fs::File;
-use std::io::{BufRead, BufReader, Lines};
+use std::io::{BufRead, BufReader, Lines, Write};
 use std::path::Path;
 use std::{env, io, process::{Command, ExitStatus}};
 
@@ -7,20 +7,25 @@ use std::{env, io, process::{Command, ExitStatus}};
 struct Arguments {
     location: String,
     old_version: String,
-    new_version: String
+    new_version: String,
+    commit_flag: bool
 }
 
 impl Arguments {
     fn new(args: &Vec<String>) -> Result<Self, &'static str> {
         if args.len() < 4 {
-            return Err("error please provide arguments [gitops_repo_location old_version new_version]")
+            return Err("error please provide required arguments [gitops_repo_location old_version new_version]")
         }
 
         let location = args[1].clone();
         let old_version = args[2].clone();
         let new_version  = args[3].clone();
+        let mut commit_flag = true;
+        if args[4].clone() == "ng" {
+            commit_flag = false;
+        }
 
-        Ok(Arguments{location, old_version, new_version})
+        Ok(Arguments{location, old_version, new_version, commit_flag})
     }
 }
 
@@ -35,19 +40,29 @@ fn main() -> io::Result<()> {
     });
 
     let lines = read_file_content(&cli_args.location)?;
+    let mut new_lines: Vec<String> = Vec::new();
     for line in lines {
-        if let Ok(line_content) = line {
-            println!("{}", line_content);
+        if let Ok(mut line_content) = line {
+            if line_content.contains(&cli_args.old_version) {
+                line_content = line_content.replace(&cli_args.old_version, &cli_args.new_version);
+            }
+            new_lines.push(line_content);
         }
     }
 
-    // commit_changes(location);
+    write_to_file(&cli_args.location, new_lines).unwrap_or_else(|err| {
+        eprintln!("{}", err);
+        std::process::exit(1);
+    });
+
+    if cli_args.commit_flag {
+        if commit_changes(&cli_args.location, &cli_args.new_version).success() {
+            // we good to push
+            assert!(push_changes(&cli_args.location).success());
+        }
+    }
     dbg!(&args);
     Ok(())
-    // if commit_changes(location).success() {
-    //     // we good to push
-    //     assert!(push_changes().success());
-    // }
 }
 
 /// Reads the content from a file line by line.
@@ -58,9 +73,9 @@ fn main() -> io::Result<()> {
 ///
 /// # Returns
 ///
-/// This function returns an `io::Result` containing an iterator over the lines of the file.
-/// Each item of the iterator is a `Result` where `Ok` is a line in the file
-/// and `Err` is an `io::Error`.
+/// This function returns an [`io::Result`] containing an iterator over the lines of the file.
+/// Each item of the iterator is a [`Result`] where [`Ok`] is a line in the file
+/// and [`Err`] is an [`io::Error`].
 /// ```
 fn read_file_content<P>(file_path: P) -> io::Result<Lines<BufReader<File>>>
 where
@@ -70,37 +85,56 @@ where
     Ok(BufReader::new(file).lines())
 }
 
-
-/// Commits changes in defined repo based on location
+/// Writes content to file
 ///
 /// # Arguments
-///
-/// * `location` - Path to repository
+/// * `location` - Path to write to.
+/// * `file_content` - File content to write as a [`Vec<String>`] vector.
 ///
 /// # Returns
 ///
-/// `ExitCode` - Exit code of the git commit command
-///
-fn commit_changes(location: &String) -> ExitStatus {
-    let mut commit = Command::new("git");
-    commit.args(["-C", location, "commit", "-am", "feat: bumped version"]);
-    let commit_status = commit.status().expect("error while getting exit code");
-    commit_status
+/// Function returns a [`io::Result`].
+/// If an error occurs while writing to file, a [`Err`] is returned.
+/// Upon successful writing to file [`Ok`] is returned
+fn write_to_file(file_path: &String, file_content: Vec<String>) -> io::Result<()> {
+    let file = File::create(file_path);
+    file?.write_all((file_content.join("\n")).as_ref())
 }
 
-/// Pushes commited changes upstream in defined repo based on location
+
+/// Commits changes in defined repo based on `location`
 ///
 /// # Arguments
 ///
-/// * `location` - Path to repository
+/// * `location` - Path to changed file
 ///
 /// # Returns
 ///
-/// `ExitCode` - Exit code of the git push command
+/// [`ExitCode`] - Exit code of the git commit command
+///
+fn commit_changes(location: &String, new_version: &String) -> ExitStatus {
+    let mut commit = Command::new("git");
+    let file_path = Path::new(location);
+    let parent_path = file_path.parent().unwrap();
+    commit.args(["-C", parent_path.to_str().unwrap(), "commit", "-am", concat!("feat: bumped version ")]);
+    commit.status().expect("error while getting exit code")
+}
+
+/// Pushes commited changes upstream in defined repo based on `location`
+///
+/// # Arguments
+///
+/// * `location` - Path to changed file
+///
+/// # Returns
+///
+/// [`ExitCode`] - Exit code of the git push command
 ///
 fn push_changes(location: &String) -> ExitStatus {
     let mut push = Command::new("git");
-    push.args(["-C", location, "push"]);
-    return push.status().expect("error while getting exit code");
+    let file_path = Path::new(location);
+    let parent_path = file_path.parent().unwrap();
+    push.args(["-C", parent_path.to_str().unwrap(), "push"]);
+    push.status().expect("error while getting exit code")
 }
 
