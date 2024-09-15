3082265884041a3929498f4033e45481557edd62
Danilo Jakob <danilojakob@protonmail.com>
2024-09-15 17:26:36 +0200

    feat: implemented reading and displaying toml files for frust project config. feat: version is now updated based on frust project config. feat: use table widget for ui with navigation

--- src/main.rs
+++ src/main.rs
@@ -1,17 +1,19 @@
-use std::fs::File;
+use core::panic;
+use std::fs::{self, File};
 use std::io::{BufRead, BufReader, Lines, Read, Write};
 use std::path::Path;
+use std::vec;
 use std::{
     io,
     process::{Command, ExitStatus},
 };
 
 use ratatui::crossterm::event::{self, Event, KeyCode, KeyEventKind};
-use ratatui::layout::Alignment;
+use ratatui::layout::{Alignment, Constraint};
+use ratatui::style::{Style, Stylize};
 use ratatui::symbols::border;
-use ratatui::text::Text;
 use ratatui::widgets::block::Title;
-use ratatui::widgets::{Block, Paragraph};
+use ratatui::widgets::{Block, Row, Table, TableState};
 use ratatui::DefaultTerminal;
 use serde::Deserialize;
 
@@ -35,35 +37,99 @@ fn main() -> io::Result<()> {
 
 fn run(mut terminal: DefaultTerminal) -> io::Result<()> {
     //let home_path = env::var("HOME").unwrap();
-    let configs_path: String =
-        "/home/churrer/Documents/github/frust/test/churrer.xyz.toml".to_owned();
-    let config = read_config_toml(&configs_path);
+    let configs = get_configs("/home/churrer/Documents/github/frust/test");
+    let mut rows: Vec<Row> = vec![];
+    let widths = [Constraint::Length(50), Constraint::Length(100)];
+
+    for test in configs.iter() {
+        rows.push(Row::new(vec![
+            test.name.clone(),
+            test.location.location.clone(),
+        ]));
+    }
+    let mut table_state = TableState::default();
     loop {
         terminal.draw(|frame| {
             let header_text = Title::from(" Frust ");
             let block = Block::bordered()
                 .title(header_text.alignment(Alignment::Center))
                 .border_set(border::THICK);
-            let content = Paragraph::new(Text::from(config.name.clone()))
-                .centered()
+            let table = Table::new(rows.to_owned(), widths)
+                .column_spacing(1)
+                .header(
+                    Row::new(vec!["Config Name", "Config Location"])
+                        .style(Style::new().bold())
+                        .bottom_margin(1),
+                )
+                .highlight_style(Style::new().reversed())
                 .block(block);
-            frame.render_widget(content, frame.area());
+            frame.render_stateful_widget(table, frame.area(), &mut table_state);
         })?;
         if let Event::Key(key) = event::read()? {
             if key.kind == KeyEventKind::Press && key.code == KeyCode::Char('q') {
                 return Ok(());
             }
+            if key.kind == KeyEventKind::Press && key.code == KeyCode::Char('j') {
+                table_state.scroll_down_by(1);
+            }
+            if key.kind == KeyEventKind::Press && key.code == KeyCode::Char('k') {
+                table_state.scroll_up_by(1);
+            }
+            if key.kind == KeyEventKind::Press && key.code == KeyCode::Char('l') {
+                let selected_row_index = table_state.selected().unwrap();
+                let selected_config = &configs[selected_row_index];
+                update_file_version(selected_config, &String::from("churrer.xyz:1.0.0"));
+            }
         }
     }
 }
 
-fn read_config_toml(file_path: &String) -> Config {
+fn get_configs(config_directory: &'static str) -> Vec<Config> {
+    let toml_paths = fs::read_dir(config_directory).unwrap();
+    let mut return_paths: Vec<String> = vec![];
+    let mut configs: Vec<Config> = vec![];
+
+    for toml_path in toml_paths {
+        let raw_path = toml_path.unwrap().path().to_str().unwrap().to_string();
+        if raw_path.contains(".toml") {
+            return_paths.push(raw_path);
+        }
+    }
+    for path in return_paths {
+        configs.push(read_config_toml(path));
+    }
+    configs
+}
+
+fn read_config_toml(file_path: String) -> Config {
     let mut file = File::open(file_path).expect("error");
     let mut buf = String::new();
     file.read_to_string(&mut buf)
         .expect("Error while reading file to string");
     let config: Config = toml::from_str(&buf).unwrap();
     config
+}
+
+fn update_file_version(config: &Config, new_version: &String) {
+    let lines = read_file_content(&config.location.location);
+    let lines = match lines {
+        Ok(buffer) => buffer,
+        Err(_) => panic!("Erro while reading file content, make sure path is valid"),
+    };
+    let mut new_lines: Vec<String> = vec![];
+    for line in lines {
+        let mut line_content = line.unwrap();
+        if line_content.contains(&config.location.previous_version) {
+            line_content = line_content.replace(&config.location.previous_version, new_version);
+        }
+        new_lines.push(line_content);
+    }
+
+    let result = write_to_file(&config.location.location, new_lines);
+    match result {
+        Ok(_) => (),
+        Err(_) => eprint!("Error"),
+    }
 }
 
 /// Reads the content from a file line by line.
