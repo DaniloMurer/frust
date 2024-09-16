3793231a30d8e3433acbff328f4df7fa9d4db9d9
Danilo Jakob <danilojakob@protonmail.com>
2024-09-16 19:53:24 +0200

    dev: input field

--- src/frust/tui/mod.rs
+++ src/frust/tui/mod.rs
@@ -32,6 +32,73 @@ impl Key {
     }
 }
 
+#[allow(clippy::upper_case_acronyms)]
+enum InputMode {
+    EDITING,
+    NORMAL,
+}
+
+struct InputField {
+    input: String,
+    character_index: usize,
+    input_mode: InputMode,
+}
+
+impl InputField {
+    const fn new() -> Self {
+        Self {
+            input: String::new(),
+            input_mode: InputMode::NORMAL,
+            character_index: 0,
+        }
+    }
+
+    fn move_cursor_left(&mut self) {
+        let cursor_moved_left = self.character_index.saturating_sub(1);
+        self.character_index = self.clamp_cursor(cursor_moved_left);
+    }
+
+    fn move_cursor_right(&mut self) {
+        let cursor_moved_left = self.character_index.saturating_add(1);
+        self.character_index = self.clamp_cursor(cursor_moved_left);
+    }
+
+    fn enter_char(&mut self, new_char: char) {
+        let index = self.byte_index();
+        self.input.insert(index, new_char);
+        self.move_cursor_right();
+    }
+
+    fn delete_char(&mut self) {
+        let is_not_cursor_leftmost = self.character_index != 0;
+        if is_not_cursor_leftmost {
+            let current_index = self.character_index;
+            let from_left_to_current_index = current_index - 1;
+            let before_char_to_delete = self.input.chars().take(from_left_to_current_index);
+            let after_char_to_delete = self.input.chars().skip(current_index);
+
+            self.input = before_char_to_delete.chain(after_char_to_delete).collect();
+            self.move_cursor_left();
+        }
+    }
+
+    fn byte_index(&self) -> usize {
+        self.input
+            .char_indices()
+            .map(|(i, _)| i)
+            .nth(self.character_index)
+            .unwrap_or(self.input.len())
+    }
+
+    fn clamp_cursor(&self, new_cursor_position: usize) -> usize {
+        new_cursor_position.clamp(0, self.input.chars().count())
+    }
+
+    fn reset_cursor(&mut self) {
+        self.character_index = 0;
+    }
+}
+
 pub struct App {
     table_state: TableState,
     title: &'static str,
